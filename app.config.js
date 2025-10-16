36d7e6fb1d83669ece25026e03dbb78880a54800
OnlyF0uR <29165327+OnlyF0uR@users.noreply.github.com>
2025-10-16 19:24:07 +0200

    new build attempt

--- app.config.js
+++ app.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from "@solidjs/start/config";
 export default defineConfig({
   server: {
     preset: "cloudflare_module",
-    compatibilityDate: "2025-10-11"
+    compatibilityDate: "2025-10-11",
+    rollupConfig: {
+      external: ["node:async_hooks"]
+    }
   }
 });
